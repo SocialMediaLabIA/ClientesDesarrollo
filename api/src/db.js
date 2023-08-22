42355c3617be07951afcb587d5f744a1b20c6148
Jonavoe <voeffray.jonathan@gmail.com>
2023-08-22 20:22:23 -0300

    Update db.js

--- api/src/db.js
+++ api/src/db.js
@@ -1,47 +1,19 @@
-require("dotenv").config();
-const { Sequelize } = require("sequelize");
-const fs = require("fs");
-const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_BDD } = process.env;
-const AdminFunction = require("./models/Admin");
-const ClientFunction = require("./models/Client");
+require('dotenv').config();
 
-const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_BDD}`,
-  {
-    logging: false,
-    native: false,
-  }
-);
-const basename = path.basename(__filename);
+const mongoose = require('mongoose');
 
-const modelDefiners = [];
+const { URI } = process.env;
+const DB_URI = URI;
 
-fs.readdirSync(path.join(__dirname, "/models"))
-  .filter(
-    (file) =>
-      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
-  )
-  .forEach((file) => {
-    modelDefiners.push(require(path.join(__dirname, "/models", file)));
-  });
-
-modelDefiners.forEach((model) => model(sequelize));
-let entries = Object.entries(sequelize.models);
-let capsEntries = entries.map((entry) => [
-  entry[0][0].toUpperCase() + entry[0].slice(1),
-  entry[1],
-]);
-sequelize.models = Object.fromEntries(capsEntries);
-
-AdminFunction(sequelize);
-ClientFunction(sequelize);
-const { Admin, Client } = sequelize.models;
-
-Client.belongsToMany(Admin, { through: "StatusClient" });
-Admin.belongsToMany(Client, { through: "StatusClient" });
-
-module.exports = {
-  ...sequelize.models,
-  conn: sequelize,
-};
+mongoose
+	.connect(DB_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => {
+		console.log('********* CONEXIÓN CORRECTA *******');
+	})
+	.catch((err) => {
+		console.log('********* ERROR DE CONEXIÓN *******');
+		console.error(err);
+	});
