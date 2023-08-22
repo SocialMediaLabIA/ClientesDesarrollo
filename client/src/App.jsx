46201b03cabe1d89e1c6f280130bd4e5e0ceb51a
Nikitoo17 <nicolasfadel17@gmail.com>
2023-08-22 17:49:12 -0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./views/Home";
-import Login from "./views/Login";
+import LoginView from "./views/LoginView";
 
 function App() {
   return (
     <>
       <Routes>
         {/* <Route path="/" element={<Home/>} /> */}
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<LoginView />} />
       </Routes>
     </>
   );
