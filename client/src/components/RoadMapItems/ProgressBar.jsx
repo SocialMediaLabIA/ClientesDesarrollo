29e6c01b06a36a1d958e7e306f3ac8c726275c11
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-08-22 16:45:48 -0300

    Update ProgressBar.jsx

--- client/src/components/RoadMapItems/ProgressBar.jsx
+++ client/src/components/RoadMapItems/ProgressBar.jsx
@@ -2,22 +2,34 @@ import React from "react";
 
 export default function ProgressBar() {
   return (
-    <div>
-      <div>
-        <p>1</p>
-      </div>
-      <div>
-        <p>2</p>
-      </div>
-      <div>
-        <p>3</p>
-      </div>
-      <div>
-        <p>4</p>
-      </div>
-      <div>
-        <p>5</p>
-      </div>
+    <div className="w-full flex justify-center items-center border-4 text-white mt-10">
+      <p className="border-4 rounded-full w-10 h-10 flex justify-center items-center">
+        1
+      </p>
+      <div className="border-4 w-2"></div>
+      <div className="border-4 w-2  m-1"></div>
+      <div className="border-4 w-2"></div>
+      <p className="border-4 rounded-full w-10 h-10 flex justify-center items-center">
+        2
+      </p>
+      <div className="border-4 w-2"></div>
+      <div className="border-4 w-2 m-1"></div>
+      <div className="border-4 w-2"></div>
+      <p className="border-4 rounded-full w-10 h-10 flex justify-center items-center">
+        3
+      </p>
+      <div className="border-4 w-2"></div>
+      <div className="border-4 w-2 m-1"></div>
+      <div className="border-4 w-2"></div>
+      <p className="border-4 rounded-full w-10 h-10 flex justify-center items-center">
+        4
+      </p>
+      <div className="border-4 w-2"></div>
+      <div className="border-4 w-2 m-1"></div>
+      <div className="border-4 w-2"></div>
+      <p className="border-4 rounded-full w-10 h-10 flex justify-center items-center">
+        5
+      </p>
     </div>
   );
 }
