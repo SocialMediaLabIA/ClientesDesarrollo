7adc927314b83e12b05700d7841073cd853a6d89
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-08-22 16:13:24 -0300

    Create ProgressBar.jsx

---
+++ client/src/components/RoadMapItems/ProgressBar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function ProgressBar() {
+  return (
+    <div>
+      <div>
+        <p>1</p>
+      </div>
+      <div>
+        <p>2</p>
+      </div>
+      <div>
+        <p>3</p>
+      </div>
+      <div>
+        <p>4</p>
+      </div>
+      <div>
+        <p>5</p>
+      </div>
+    </div>
+  );
+}
