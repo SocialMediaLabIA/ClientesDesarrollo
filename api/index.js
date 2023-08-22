7993b5dcd1e55f67d0a6b41b5dbc7b16e0cbfd44
Jonavoe <voeffray.jonathan@gmail.com>
2023-08-22 16:50:24 -0300

    Create index.js

---
+++ api/index.js
@@ -0,0 +1,11 @@
+const server = require("./src/app.js");
+const { conn } = require("./src/db.js");
+require("dotenv").config();
+
+const { PORT } = process.env;
+
+conn.sync({ alter: false }).then(() => {
+  server.listen(PORT, () => {
+    console.log(`{%s listening at ${PORT}}`);
+  });
+});
