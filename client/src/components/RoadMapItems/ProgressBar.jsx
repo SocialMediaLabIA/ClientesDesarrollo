11f2ec594eb39efb4c3ae7aa81fea1ebba1eb120
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-08-22 16:58:35 -0300

    Update ProgressBar.jsx

--- client/src/components/RoadMapItems/ProgressBar.jsx
+++ client/src/components/RoadMapItems/ProgressBar.jsx
@@ -1,32 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function ProgressBar() {
+const [progress, setProgress] = useState([0,0])
+
   return (
     <div className="w-full flex justify-center items-center border-4 text-white mt-10">
       <p className="border-4 rounded-full w-10 h-10 flex justify-center items-center">
         1
       </p>
-      <div className="border-4 w-2"></div>
-      <div className="border-4 w-2  m-1"></div>
-      <div className="border-4 w-2"></div>
+      <div className="border-2 w-2"></div>
+      <div className="border-2 w-2  m-1"></div>
+      <div className="border-2 w-2"></div>
       <p className="border-4 rounded-full w-10 h-10 flex justify-center items-center">
         2
       </p>
-      <div className="border-4 w-2"></div>
-      <div className="border-4 w-2 m-1"></div>
-      <div className="border-4 w-2"></div>
+      <div className="border-2 w-2"></div>
+      <div className="border-2 w-2 m-1"></div>
+      <div className="border-2 w-2"></div>
       <p className="border-4 rounded-full w-10 h-10 flex justify-center items-center">
         3
       </p>
-      <div className="border-4 w-2"></div>
-      <div className="border-4 w-2 m-1"></div>
-      <div className="border-4 w-2"></div>
+      <div className="border-2 w-2"></div>
+      <div className="border-2 w-2 m-1"></div>
+      <div className="border-2 w-2"></div>
       <p className="border-4 rounded-full w-10 h-10 flex justify-center items-center">
         4
       </p>
-      <div className="border-4 w-2"></div>
-      <div className="border-4 w-2 m-1"></div>
-      <div className="border-4 w-2"></div>
+      <div className="border-2 w-2"></div>
+      <div className="border-2 w-2 m-1"></div>
+      <div className="border-2 w-2"></div>
       <p className="border-4 rounded-full w-10 h-10 flex justify-center items-center">
         5
       </p>
