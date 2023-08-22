4a4c60d5495fe7565837b673f3d2767fe9b3e68d
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-08-22 15:47:51 -0300

    Create RoadMap.jsx

---
+++ client/src/views/RoadMap.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function RoadMap() {
+  return (
+    <div className='flex justify-center items-center h-screen'>
+        <h1>Bienvenidos a SocialMediaLab</h1>
+    </div>
+  )
+}
