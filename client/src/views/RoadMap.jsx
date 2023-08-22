810e4980ce1fc8a0a81fdc0e3d672188a6ebf001
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-08-22 16:13:26 -0300

    Update RoadMap.jsx

--- client/src/views/RoadMap.jsx
+++ client/src/views/RoadMap.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React from "react";
+import { HiOutlineUserCircle } from "react-icons/hi";
+import logo from "../assets/smllogo.webp";
 
 export default function RoadMap() {
   return (
-    <div className='flex justify-center items-center h-screen'>
-        <div className='flex justify-between items-center'>
-        <div>sssss</div>
-        <h1>Bienvenidos a SocialMediaLab AI</h1>
-        <div>ssssss</div>
-        </div>
+    <div className="flex justify-center items-start h-screen w-screen">
+      <div className="flex justify-between items-center w-4/5 mt-8">
+        <img src={logo} alt="Logo" className="w-12 h-12" />
+        <h1 className="text-base">Social Media Lab</h1>
+        <HiOutlineUserCircle className="w-12 h-12" />
+      </div>
+      
     </div>
-  )
+  );
 }
