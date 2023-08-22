231fe2db25e7186ea08618838e065b14b4bc4736
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-08-22 15:11:03 -0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import Home from "./components/Home";
+import { Routes, Route, useLocation } from "react-router-dom";
 import "./App.css";
+import Home from "./views/Home";
+
 
 function App() {
   const [count, setCount] = useState(0);
