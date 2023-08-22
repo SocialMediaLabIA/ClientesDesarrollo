ee8ce0d303012ec221097d5cdbeca58662f0c5a6
Nikitoo17 <nicolasfadel17@gmail.com>
2023-08-22 17:49:19 -0300

    Create LoginView.jsx

---
+++ client/src/views/LoginView.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import Login from "../components/login/Login";
+
+export default function LoginView() {
+  return (
+    <div className="flex justify-center items-center bg-black h-screen w-screen">
+      <Login />
+    </div>
+  );
+}
