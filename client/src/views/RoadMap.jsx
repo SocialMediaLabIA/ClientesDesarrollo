ad022509e1ab49fc3eaac9abf6b13cc1ddb00e9b
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-08-22 17:02:55 -0300

    Update RoadMap.jsx

--- client/src/views/RoadMap.jsx
+++ client/src/views/RoadMap.jsx
@@ -4,6 +4,13 @@ import logo from "../assets/smllogo.webp";
 import ProgressBar from "../components/RoadMapItems/ProgressBar";
 export default function RoadMap() {
   const [progress, setProgress] = useState([0, 0]);
+
+  const directionProgress = (direction) => {
+if(direction === "next") setProgress(++progress)
+if(direction === "prev") setProgress(--progress)
+  }
+console.log(progress)
+
   return (
     <div className="flex flex-col justify-between items-center h-screen w-screen bg-black">
       <div className="flex flex-col justify-between items-center h-4/5 w-screen bg-black m-16">
@@ -23,10 +30,10 @@ export default function RoadMap() {
           className="mt-10 w-5/6"
         />
         <div className="flex justify-between items-center w-4/5 mt-8">
-          <button class="bg-transparent  text-blue-700 font-semibold  py-2 px-4 border border-blue-500  rounded">
+          <button onClick={() => directionProgress("next")} class="bg-transparent  text-blue-700 font-semibold  py-2 px-4 border border-blue-500  rounded">
             Prev
           </button>
-          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          <button onClick={() => directionProgress("prev")} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
             Next
           </button>
         </div>
