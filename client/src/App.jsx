4216f7d4b7ea50202d93abbdff02bc76f070ac89
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-08-22 15:47:49 -0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
 import { Routes, Route} from "react-router-dom";
 import "./App.css";
 import Home from "./views/Home";
-
+import RoadMap from "./views/RoadMap";
 
 function App() {
   return (
 <>
       <Routes>
         <Route path="/" element={<Home/>} />
+        <Route path="/roadmap" element={<RoadMap/>} />
       </Routes>
 </>
   );
