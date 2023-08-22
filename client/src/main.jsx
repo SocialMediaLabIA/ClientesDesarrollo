64413cee7e1e7836ab92a21e5c27ee386f3075fd
Nikitoo17 <nicolasfadel17@gmail.com>
2023-08-22 14:52:46 -0300

    Create main.jsx

---
+++ client/src/main.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import App from "./App.jsx";
+import "./index.css";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import axios from "axios";
+
+const getServerUrl = () => {
+  const { hostname } = window.location;
+
+  if (hostname.includes("localhost")) {
+    return "http://localhost:3001/api";
+  } else if (hostname.includes("sml-app.vercel")) {
+    return "https://smlapp.onrender.com/api";
+  } else if (hostname.includes("sml-app.com")) {
+    return "https://sml-app.com/api";
+  }
+
+  return "http://localhost:3001/api";
+};
+
+axios.defaults.baseURL = getServerUrl();
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
