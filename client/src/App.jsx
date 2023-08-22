1c3f991d5ba3ed8ee2a43012993e135b436ab258
Jonavoe <voeffray.jonathan@gmail.com>
2023-08-22 20:22:48 -0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,16 @@
-import { Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./views/Home";
-
+import Admin from "./views/Admin";
 
 function App() {
   return (
-<>
+    <>
       <Routes>
-        <Route path="/" element={<Home/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<Admin />} />
       </Routes>
-</>
+    </>
   );
 }
 
