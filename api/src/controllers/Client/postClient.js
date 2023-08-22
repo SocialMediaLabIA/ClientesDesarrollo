0d5e69d2511141f686850d127e3ab49672b2e319
Jonavoe <voeffray.jonathan@gmail.com>
2023-08-22 20:22:21 -0300

    Delete postClient.js

--- api/src/controllers/Client/postClient.js
+++
@@ -1,8 +0,0 @@
-const { Client } = require("../../db");
-
-const postClient = async (name) => {
-  const newClient = await Client.create({ name });
-  return newClient;
-};
-
-module.exports = { postClient };
