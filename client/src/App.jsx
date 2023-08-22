55c8d76126afb877a47ccfdc2c6bb6e165364609
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-08-22 15:34:40 -0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
+import { Routes, Route} from "react-router-dom";
 import "./App.css";
+import Home from "./views/Home";
+
 
 function App() {
-  return <div></div>;
+  return (
+<>
+      <Routes>
+        <Route path="/" element={<Landing/>} />
+        <Route path="/home" element={<Home/>} />
+        <Route path="/detail/:id" element={<Detail/>} />
+        <Route path="/create" element={<Create/>} />
+        <Route path="/about" element={<About/>} />
+      </Routes>
+</>
+  );
 }
 
 export default App;
