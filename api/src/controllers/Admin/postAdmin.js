865cf004fa4a54eaefb7e967671f4074a75e51a3
Jonavoe <voeffray.jonathan@gmail.com>
2023-08-22 20:21:59 -0300

    Delete postAdmin.js

--- api/src/controllers/Admin/postAdmin.js
+++
@@ -1,8 +0,0 @@
-const { Admin } = require("../../db");
-
-const postAdmin = async (name) => {
-  const newAdmin = await Admin.create({ name });
-  return newAdmin;
-};
-
-module.exports = { postAdmin };
