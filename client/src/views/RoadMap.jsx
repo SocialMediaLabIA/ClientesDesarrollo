d4737f77b0a7d7b4b42e6d4bbcd67c5aa0e659d1
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-08-22 15:55:29 -0300

    Update RoadMap.jsx

--- client/src/views/RoadMap.jsx
+++ client/src/views/RoadMap.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 export default function RoadMap() {
   return (
     <div className='flex justify-center items-center h-screen'>
-        <h1>Bienvenidos a SocialMediaLab</h1>
+        <div className='flex justify-between items-center'>
+        <div>sssss</div>
+        <h1>Bienvenidos a SocialMediaLab AI</h1>
+        <div>ssssss</div>
+        </div>
     </div>
   )
 }
