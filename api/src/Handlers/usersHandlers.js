0a0f03d171429ad3fc0021d803a9d42ecab14028
Nikitoo17 <nicolasfadel17@gmail.com>
2023-08-23 16:55:20 -0300

    Update usersHandlers.js

--- api/src/Handlers/usersHandlers.js
+++ api/src/Handlers/usersHandlers.js
@@ -1,3 +1,4 @@
+const getUserByEmail = require("../controllers/users/getUserByEmail");
 const postUsers = require("../controllers/users/postUsers");
 
 const postUsersHandler = async (req, res) => {
@@ -10,6 +11,17 @@ const postUsersHandler = async (req, res) => {
   }
 };
 
+const getUserByEmailHandler = async (req, res) => {
+  const { email } = req.query;
+  try {
+    const User = await getUserByEmail(email);
+    res.status(200).json(User);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+};
+
 module.exports = {
   postUsersHandler,
+  getUserByEmailHandler,
 };
