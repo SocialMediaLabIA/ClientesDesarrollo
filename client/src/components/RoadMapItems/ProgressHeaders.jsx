6748343fef0480449a578290a2551a878be36bb7
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-08-23 18:19:15 -0300

    Update ProgressHeaders.jsx

--- client/src/components/RoadMapItems/ProgressHeaders.jsx
+++ client/src/components/RoadMapItems/ProgressHeaders.jsx
@@ -1,10 +1,8 @@
 import React from "react";
-import imageMeet from "../../assets/fotomeet.png" 
 import pages from "../../utils/progressPages";
 
 export default function ProgressHeaders({ progress }) {
-
-console.log(pages[progress].video)
+  console.log(pages[progress].video);
   return (
     <div className="flex justify-center items-center w-full h-5/5 mt-12 text-white">
       {pages[progress].video === true && (
@@ -18,30 +16,21 @@ console.log(pages[progress].video)
           className="mt-5 w-5/6"
         />
       )}
+      {pages[progress].resume === true && pages[progress].number !== 20 && (
+        <p className="mt-10 w-5/6 text-justify items-center">{pages[progress].resumeText}</p>
+      )}
       {pages[progress].meet === true && (
-        <iframe
-          title="YouTube Video"
-          width="560"
-          height="315"
-          src={pages[progress].videoSource} // Cambia esta URL por la URL correcta del video embebido
-          frameBorder="0" // Cambiado a frameBorder
-          allowFullScreen // Cambiado a allowFullScreen
-          className="mt-10 w-5/6"
-        />
+        <div className="flex justify-center items-center h-full w-full ">
+          <img
+            src={pages[progress].meetImage}
+            alt="imagen meet"
+            className="w-60"
+          />
+        </div>
       )}
-      {pages[progress].resume === true && (
-        <p
-          className="mt-10 w-5/6 text-justify"
-        >{pages[progress].resumeText}</p>
+      {pages[progress].number === 20 && (
+        <p className="mt-10 w-5/6 text-center">{pages[progress].resumeText}</p>
       )}
-          {pages[progress].meet === true && (
-            <img src={imageMeet} alt="imagen meet" className="w-full border-2 h-40 " />
-          )}
-      {pages[progress].number === 13 && (
-          <p
-          className="mt-10 w-5/6 text-center"
-          >{pages[progress].resumeText}</p>
-          )}
     </div>
   );
 }
