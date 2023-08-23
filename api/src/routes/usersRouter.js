abc206d6a1f9340e08464bb53a6f1ed5bbac7122
Nikitoo17 <nicolasfadel17@gmail.com>
2023-08-23 18:23:32 -0300

    Update usersRouter.js

--- api/src/routes/usersRouter.js
+++ api/src/routes/usersRouter.js
@@ -2,11 +2,13 @@ const { Router } = require("express");
 const {
   postUsersHandler,
   getUserByLoginHandler,
+  setAccessUserHandler,
 } = require("../Handlers/usersHandlers");
 
 const UsersRouter = Router();
 
 UsersRouter.get("/email", getUserByLoginHandler);
+UsersRouter.put("/access", setAccessUserHandler);
 UsersRouter.post("/", postUsersHandler);
 
 module.exports = UsersRouter;
