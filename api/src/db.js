4e8fc49ae7f97570e982da2e73f62e098d36c12f
Nikitoo17 <nicolasfadel17@gmail.com>
2023-08-23 16:55:17 -0300

    Update db.js

--- api/src/db.js
+++ api/src/db.js
@@ -1,19 +1,22 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const { URI } = process.env;
 const DB_URI = URI;
 
+console.log(DB_URI);
+
 mongoose
-	.connect(DB_URI, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	})
-	.then(() => {
-		console.log('********* CONEXIÓN CORRECTA *******');
-	})
-	.catch((err) => {
-		console.log('********* ERROR DE CONEXIÓN *******');
-		console.error(err);
-	});
+  .connect(DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("********* CONEXIÓN CORRECTA *******");
+    console.log(DB_URI);
+  })
+  .catch((err) => {
+    console.log("********* ERROR DE CONEXIÓN *******");
+    console.error(err);
+  });
