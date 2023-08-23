df9645743f83832626f451392d935586e8ba1abb
Jonavoe <94918075+Jonavoe@users.noreply.github.com>
2023-08-23 16:20:54 -0300

    Update contentsHandlers.js

--- api/src/Handlers/contentsHandlers.js
+++ api/src/Handlers/contentsHandlers.js
@@ -1,15 +1,15 @@
-const postUsers = require("../controllers/users/postUsers");
+const postContents = require("../controllers/contents/postContents");
 
-const postUsersHandler = async (req, res) => {
+const postContentsHandler = async (req, res) => {
   const body = req.body;
   try {
-    const Users = await postUsers(body);
-    res.status(200).json(Users);
+    const contents = await postContents(body);
+    res.status(200).json(contents);
   } catch (error) {
     res.status(404).json({ error: error.message });
   }
 };
 
 module.exports = {
-  postUsersHandler,
+  postContentsHandler,
 };
