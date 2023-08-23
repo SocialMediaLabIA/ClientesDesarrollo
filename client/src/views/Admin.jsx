76780be9fb5c95b2a2691736b3171b8f84ecbb1f
Jonavoe <voeffray.jonathan@gmail.com>
2023-08-23 20:03:08 -0300

    Update Admin.jsx

--- client/src/views/Admin.jsx
+++ client/src/views/Admin.jsx
@@ -1,9 +1,9 @@
-import ListAdmin from "../components/Admin/ListAdmin";
+import Users from "../components/Admin/Users/Users";
 
 export default function Admin() {
   return (
-    <div>
-      <ListAdmin />
+    <div className="w-screen h-screen text-white bg-black">
+      <Users />
     </div>
   );
 }
