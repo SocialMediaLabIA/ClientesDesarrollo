b437d432401f1bebfc47eb75fe9a3a55674fc72b
Jonavoe <voeffray.jonathan@gmail.com>
2023-08-23 20:02:53 -0300

    Delete sectionsRouter.js

--- api/src/routes/sectionsRouter.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-const { postSectionsHandler } = require("../Handlers/sectionsHandlers");
-
-const SectionsRouter = Router();
-
-SectionsRouter.post("/", postSectionsHandler);
-
-module.exports = SectionsRouter;
