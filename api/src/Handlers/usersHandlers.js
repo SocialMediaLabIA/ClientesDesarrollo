c4a11d94619c5022d4ff8e28314703563701b707
Jonavoe <voeffray.jonathan@gmail.com>
2023-08-23 17:10:58 -0300

    Update usersHandlers.js

--- api/src/Handlers/usersHandlers.js
+++ api/src/Handlers/usersHandlers.js
@@ -1,3 +1,4 @@
+const getAllUser = require("../controllers/users/getAllUsers");
 const getUserByEmail = require("../controllers/users/getUserByEmail");
 const postUsers = require("../controllers/users/postUsers");
 
@@ -5,6 +6,15 @@ const postUsersHandler = async (req, res) => {
   const body = req.body;
   try {
     const Users = await postUsers(body);
+    res.status(200).json(Users);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+};
+
+const getAllUserHandler = async (req, res) => {
+  try {
+    const Users = await getAllUser();
     res.status(200).json(Users);
   } catch (error) {
     res.status(404).json({ error: error.message });
@@ -23,5 +33,6 @@ const getUserByEmailHandler = async (req, res) => {
 
 module.exports = {
   postUsersHandler,
+  getAllUserHandler,
   getUserByEmailHandler,
 };
