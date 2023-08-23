4d39dad9af3175391b3103c813bfb62ed745c712
Jonavoe <voeffray.jonathan@gmail.com>
2023-08-23 17:17:12 -0300

    Update getAllUser.js

--- client/src/redux/User/ActionUser/getAllUser.js
+++ client/src/redux/User/ActionUser/getAllUser.js
@@ -3,8 +3,8 @@ export const GET_ALL_USER = "GET_ALL_USER";
 
 export const getAllUser = () => {
   return async (dispatch) => {
-    const response = await axios.get("/user");
-    const user = response.data;
-    dispatch({ type: GET_ALL_USER, payload: user });
+    const response = await axios.get("/users");
+    const users = response.data;
+    dispatch({ type: GET_ALL_USER, payload: users });
   };
 };
