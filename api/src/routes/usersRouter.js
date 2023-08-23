617508abd5bbcd6dbbe6343f0766433aa85cf7ea
Jonavoe <voeffray.jonathan@gmail.com>
2023-08-23 17:11:01 -0300

    Update usersRouter.js

--- api/src/routes/usersRouter.js
+++ api/src/routes/usersRouter.js
@@ -2,11 +2,13 @@ const { Router } = require("express");
 const {
   postUsersHandler,
   getUserByEmailHandler,
+  getAllUserHandler,
 } = require("../Handlers/usersHandlers");
 
 const UsersRouter = Router();
 
-UsersRouter.get("/", getUserByEmailHandler);
+UsersRouter.get("/", getAllUserHandler);
+UsersRouter.get("/email", getUserByEmailHandler);
 UsersRouter.post("/", postUsersHandler);
 
 module.exports = UsersRouter;
