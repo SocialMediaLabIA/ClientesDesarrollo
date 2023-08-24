a8a7f48c602594e8c854d1673f361282993b20fa
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-08-24 15:07:03 -0300

    Update usersRouter.js

--- api/src/routes/usersRouter.js
+++ api/src/routes/usersRouter.js
@@ -3,6 +3,7 @@ const {
   postUsersHandler,
   getUserByLoginHandler,
   setAccessUserHandler,
+  setProgressUserHandler
 } = require("../Handlers/usersHandlers");
 
 const UsersRouter = Router();
@@ -10,6 +11,8 @@ const UsersRouter = Router();
 // UsersRouter.get("/", getAllUserHandler);
 UsersRouter.get("/email", getUserByLoginHandler);
 UsersRouter.put("/access", setAccessUserHandler);
+console.log("ROUTESSSSSS")
+UsersRouter.put("/progress", setProgressUserHandler);
 UsersRouter.post("/", postUsersHandler);
 
 module.exports = UsersRouter;
