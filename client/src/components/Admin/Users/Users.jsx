226ad6f694e2b4cedc7995828d248c93f3418549
Jonavoe <voeffray.jonathan@gmail.com>
2023-08-24 16:48:59 -0300

    Update Users.jsx

--- client/src/components/Admin/Users/Users.jsx
+++ client/src/components/Admin/Users/Users.jsx
@@ -1,15 +1,26 @@
+import { useState } from "react";
 import SideBar from "../SideBar/SideBar";
 import ListAdmin from "./ListUsers/ListAdmin";
+import CreateUsers from "./CreateUsers/CreateUsers";
+import Panel from "./Panel/Panel";
 
 export default function Users() {
+  const [sidebar, setSidebar] = useState("admin");
+
+  const handleSidebar = (panel) => {
+    setSidebar(panel);
+  };
+
   return (
     <div className="flex w-full h-full  ">
-      <SideBar />
+      <SideBar handleSidebar={handleSidebar} />
       <div className="flex w-full h-full  flex-col gap-5 p-10">
-        <div className=" bg-[#282828] p-5 rounded-lg">
+        <div className=" text-[1.5rem] font-semibold bg-[#282828] p-5 rounded-lg">
           <p>Panel de administrador</p>
         </div>
-        <ListAdmin />
+        {sidebar === "admin" && <Panel />}
+        {sidebar === "usuario" && <ListAdmin />}
+        {sidebar === "crearUsuario" && <CreateUsers />}
       </div>
     </div>
   );
