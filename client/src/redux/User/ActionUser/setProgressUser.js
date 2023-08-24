7bafd98021c91f31fc67556892bccf8fce255c1b
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-08-24 15:51:19 -0300

    Update setProgressUser.js

--- client/src/redux/User/ActionUser/setProgressUser.js
+++ client/src/redux/User/ActionUser/setProgressUser.js
@@ -1,7 +1,10 @@
 import axios from "axios";
+export const GET_PROGRESS_NUMBER = "GET_PROGRESS_NUMBER";
 
 export const setProgressUser = (id) => {
   return async (dispatch) => {
-    await axios.put(`/users/progress?id=${id}`);
+    const response = await axios.put(`/users/progress?id=${id}`);
+    const progress = response.data.progress;
+    dispatch({ type: GET_PROGRESS_NUMBER, payload: progress });
   };
 };
