49bb5a9451f7bf3328c1a33b31dc74a65e9eb588
Jonavoe <voeffray.jonathan@gmail.com>
2023-08-24 16:48:54 -0300

    Update ListAdmin.jsx

--- client/src/components/Admin/Users/ListUsers/ListAdmin.jsx
+++ client/src/components/Admin/Users/ListUsers/ListAdmin.jsx
@@ -2,6 +2,8 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllUser } from "../../../../redux/User/ActionUser/getAllUser";
 
+import { BsTrash, BsPencilSquare } from "react-icons/bs";
+
 export default function ListAdmin() {
   const dispatch = useDispatch();
 
@@ -11,92 +13,62 @@ export default function ListAdmin() {
     dispatch(getAllUser());
   }, [dispatch]);
 
-  const formatearFecha = (fechaISO) => {
-    const fechaObj = new Date(fechaISO);
-    const dia = fechaObj.getDate().toString().padStart(2, "0");
-    const mes = (fechaObj.getMonth() + 1).toString().padStart(2, "0");
-    const anio = fechaObj.getFullYear();
-    // const horas = fechaObj.getHours().toString().padStart(2, "0");
-    // const minutos = fechaObj.getMinutes().toString().padStart(2, "0");
-
-    return `${dia}-${mes}-${anio}`;
-  };
-
   return (
     <div className="flex flex-col gap-2 items-center justify-start w-full h-full bg-[#282828] rounded-lg">
-      <div className="flex gap-5 w-[95%] my-2 py-5 text-[1.5rem] bg-[#222222] text-center rounded-lg">
-        <div className="ml-5 w-60 min-w-[80px]">
-          <p className="flex text-center items-center justify-center">Nombre</p>
-        </div>
-        <div className="w-60 min-w-[80px]">
+      <div className="flex gap-2 w-[95%] my-2 py-5 text-[1.5rem] bg-[#222222] text-center rounded-lg">
+        <div className="w-[20rem] min-w-[80px]">
           <p className="flex text-center items-center justify-center">
-            Apellido
+            Nombre de Usuario
           </p>
         </div>
-        <div className="min-w-[224px]">
+        <div className=" w-[20rem] min-w-[224px]">
           <p className="flex text-center items-center justify-center">Email</p>
         </div>
-        <div className="w-60 min-w-[70px]">
-          <p className="flex text-center items-center justify-center">Estado</p>
-        </div>
-        <div className="w-60 min-w-[80px]">
+        <div className="w-[20rem] min-w-[70px]">
           <p className="flex text-center items-center justify-center">
-            Progreso
+            Fase Proyecto
           </p>
         </div>
-        <div className="w-60 min-w-[50px]">
-          <p className="flex text-center items-center justify-center">Rol</p>
+        <div className="w-[20rem] min-w-[80px]">
+          <p className="flex text-center items-center justify-center">
+            % completado
+          </p>
         </div>
-        <div className="mr-5 w-60  min-w-[100px]">
-          <p className="flex text-center items-center justify-center">Creado</p>
+        <div className="w-[20rem] min-w-[50px]">
+          <p className="flex text-center items-center justify-center"></p>
         </div>
       </div>
       {users &&
         users.map((item, index) => (
           <div
-            className="flex gap-5 py-2 w-[95%] text-center justify-center items-center bg-[#222222] rounded-lg  text-[1.2rem]"
+            className="flex gap-2 py-2 w-[95%] text-center justify-center items-center bg-[#222222] rounded-lg  text-[1.2rem]"
             key={index}
           >
-            <div className=" text-center ml-5 w-60 min-w-[80px]">
+            <div className=" text-center w-[20rem] min-w-[80px]">
               <p className="flex text-center items-center justify-center">
-                {item.name}
+                {item.name} {item.lastname}
               </p>
             </div>
-            <div className="w-60 min-w-[80px]">
-              <p className="flex text-center items-center justify-center">
-                {item.lastname}
-              </p>
-            </div>
-            <div className="w-60 min-w-[224px]">
+            <div className="w-[20rem] min-w-[224px]">
               <p className="flex text-center items-center justify-center">
                 {item.email}
               </p>
             </div>
-            <div className="w-60 min-w-[70px]">
-              {item.isActive ? (
-                <p className="flex text-center items-center justify-center">
-                  Activo
-                </p>
-              ) : (
-                <p className="flex text-center items-center justify-center">
-                  Inactivo
-                </p>
-              )}
-            </div>
-            <div className="w-60 min-w-[80px]">
+            <div className="w-[20rem] min-w-[80px]">
               <p className="flex text-center items-center justify-center">
                 {item.progress}
               </p>
             </div>
-            <div className="w-60 min-w-[50px]">
+            <div className="w-[20rem] min-w-[80px]">
               <p className="flex text-center items-center justify-center">
-                {item.role}
+                {(item.progress / 25) * 100}%
               </p>
             </div>
-            <div className="mr-5 w-60 min-w-[100px]">
-              <p className="flex text-center items-center justify-center">
-                {formatearFecha(item.createdAt)}
-              </p>
+            <div className="w-[20rem] min-w-[80px]">
+              <div className=" flex gap-5 items-center justify-center">
+                <BsTrash className="w-10 h-10" />
+                <BsPencilSquare className="w-10 h-10" />
+              </div>
             </div>
           </div>
         ))}
