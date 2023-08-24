d4be47a65c420d1117e10a87e9675ed73beb635d
Jonavoe <voeffray.jonathan@gmail.com>
2023-08-24 19:00:20 -0300

    Update CreateUsers.jsx

--- client/src/components/Admin/Users/CreateUsers/CreateUsers.jsx
+++ client/src/components/Admin/Users/CreateUsers/CreateUsers.jsx
@@ -4,7 +4,9 @@ export default function CreateUsers() {
   return (
     <div className="flex flex-col gap-2 items-center justify-center w-full h-full bg-[#282828] rounded-lg">
       <div className="flex flex-col items-center justify-center gap-5">
-        <h2 className="text-[2rem] font-semibold">Crear Usuario</h2>
+        <h2 className="text-[2rem] font-semibold">
+          Formulario Creación Usuario
+        </h2>
         <CreateUserForm />
       </div>
     </div>
