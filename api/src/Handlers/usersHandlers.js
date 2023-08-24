5eee6b1372ece72bcd958e3a7c70ec1a6eefd947
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-08-24 19:08:50 -0300

    Update usersHandlers.js

--- api/src/Handlers/usersHandlers.js
+++ api/src/Handlers/usersHandlers.js
@@ -1,6 +1,8 @@
 const getUserByLogin = require("../controllers/users/getUserByLogin");
 const postUsers = require("../controllers/users/postUsers");
 const setAccessUser = require("../controllers/users/setAccessUser");
+const setProgressUser = require("../controllers/users/setProgressUser");
+const getProgressUser = require("../controllers/users/getProgressUser");
 
 const postUsersHandler = async (req, res) => {
   const body = req.body;
@@ -31,8 +33,30 @@ const setAccessUserHandler = async (req, res) => {
   }
 };
 
+const getProgressUserHandler = async (req, res) => {
+  const { id } = req.query;
+  try {
+    const User = await getProgressUser(id);
+    res.status(200).json(User);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+};
+const setProgressUserHandler = async (req, res) => {
+  const { id } = req.query;
+  const {direction} = req.body;
+  try {
+    const User = await setProgressUser(id, direction);
+    res.status(200).json(User);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+};
+
 module.exports = {
   postUsersHandler,
   getUserByLoginHandler,
   setAccessUserHandler,
+  setProgressUserHandler,
+  getProgressUserHandler
 };
