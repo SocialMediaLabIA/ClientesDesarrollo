5ef52209baedbf40a3884ba2ba0d1516582ff2ca
Jonavoe <voeffray.jonathan@gmail.com>
2023-08-24 16:48:49 -0300

    Create CreateUsers.jsx

---
+++ client/src/components/Admin/Users/CreateUsers/CreateUsers.jsx
@@ -0,0 +1,12 @@
+import CreateUserForm from "./MUI/CreateUserForm";
+
+export default function CreateUsers() {
+  return (
+    <div className="flex flex-col gap-2 items-center justify-center w-full h-full bg-[#282828] rounded-lg">
+      <div className="flex flex-col items-center justify-center gap-5">
+        <h2 className="text-[2rem] font-semibold">Crear Usuario</h2>
+        <CreateUserForm />
+      </div>
+    </div>
+  );
+}
