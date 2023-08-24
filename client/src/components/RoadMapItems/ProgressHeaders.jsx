d90ef53057d78f217bbfac3d89f99f6d0f1d9d29
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-08-24 16:30:12 -0300

    Update ProgressHeaders.jsx

--- client/src/components/RoadMapItems/ProgressHeaders.jsx
+++ client/src/components/RoadMapItems/ProgressHeaders.jsx
@@ -1,35 +1,35 @@
 import React from "react";
 import pages from "../../utils/progressPages";
 
-export default function ProgressHeaders({ progress }) {
-  console.log(pages[progress].video);
+export default function ProgressHeaders({ progressNumber }) {
+  console.log(pages[progressNumber] && pages[progressNumber].video);
   return (
     <div className="flex justify-center items-center w-full h-5/5 mt-12 text-white">
-      {pages[progress].video === true && (
+      {pages[progressNumber] && pages[progressNumber].video === true && (
         <iframe
           title="YouTube Video"
           width="580"
           height="330"
-          src={pages[progress].videoSource} // Cambia esta URL por la URL correcta del video embebido
+          src={pages[progressNumber] && pages[progressNumber].videoSource} // Cambia esta URL por la URL correcta del video embebido
           frameBorder="0" // Cambiado a frameBorder
           allowFullScreen // Cambiado a allowFullScreen
           className="mt-5 w-5/6"
         />
       )}
-      {pages[progress].resume === true && pages[progress].number !== 20 && (
-        <p className="mt-10 w-5/6 text-justify items-center">{pages[progress].resumeText}</p>
+      {pages[progressNumber] && pages[progressNumber].resume === true && pages[progressNumber] && pages[progressNumber].number !== 20 && (
+        <p className="mt-10 w-5/6 text-justify items-center">{pages[progressNumber] && pages[progressNumber].resumeText}</p>
       )}
-      {pages[progress].meet === true && (
+      {pages[progressNumber] && pages[progressNumber].meet === true && (
         <div className="flex justify-center items-center h-full w-full ">
           <img
-            src={pages[progress].meetImage}
+            src={pages[progressNumber] && pages[progressNumber].meetImage}
             alt="imagen meet"
             className="w-60"
           />
         </div>
       )}
-      {pages[progress].number === 20 && (
-        <p className="mt-10 w-5/6 text-center">{pages[progress].resumeText}</p>
+      {pages[progressNumber] && pages[progressNumber].number === 20 && (
+        <p className="mt-10 w-5/6 text-center">{pages[progressNumber] && pages[progressNumber].resumeText}</p>
       )}
     </div>
   );
