326a6b1b041c2eb25700b51ecb1bd7a93718da84
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-08-24 16:03:07 -0300

    Create getProgressUser.js

---
+++ client/src/redux/User/ActionUser/getProgressUser.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+export const GET_PROGRESS_NUMBER = "GET_PROGRESS_NUMBER";
+
+export const setProgressUser = (id) => {
+  return async (dispatch) => {
+    const response = await axios.get(`/users/progress?id=${id}`);
+    const progress = response.data.progress;
+    dispatch({ type: GET_PROGRESS_NUMBER, payload: progress });
+  };
+};
