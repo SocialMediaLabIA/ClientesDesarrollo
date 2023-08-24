35b533618e84d6a6275037a8cf81cb5f7fb3a5bf
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-08-24 15:30:11 -0300

    Update setProgressUser.js

--- api/src/controllers/users/setProgressUser.js
+++ api/src/controllers/users/setProgressUser.js
@@ -1,12 +1,11 @@
 const Users = require("../../models/Users");
 
 const setProgressUser = async (id) => {
-    console.log("entrooooooooooooooo")
-//   const user = await Users.findOneAndUpdate(
-//     { _id: id },
-//     { $inc: { progress: 1 } },
-//     { new: true }
-//   );
+  const user = await Users.findOneAndUpdate(
+    { _id: id },
+    { $inc: { progress: 1 } },
+    { new: true }
+  );
   return user;
 };
 
