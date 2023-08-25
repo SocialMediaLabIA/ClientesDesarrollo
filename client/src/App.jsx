4b4f5cb18aed1fc900016079fc0e014cfa538390
Jonavoe <voeffray.jonathan@gmail.com>
2023-08-25 16:32:17 -0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import Home from "./views/Home";
 import LoginView from "./views/LoginView";
 import RoadMap from "./views/RoadMap";
 import Admin from "./views/Admin";
@@ -9,9 +8,7 @@ function App() {
   return (
     <>
       <Routes>
-        {/* <Route path="/" element={<Home/>} /> */}
         <Route path="/" element={<LoginView />} />
-        {/* <Route path="/" element={<Home />} /> */}
         <Route path="/roadmap" element={<RoadMap />} />
         <Route path="/admin" element={<Admin />} />
       </Routes>
