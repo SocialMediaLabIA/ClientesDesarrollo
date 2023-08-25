3cf6a2bded13f976e2c5645e8c88e4a003884c70
Jonavoe <voeffray.jonathan@gmail.com>
2023-08-25 17:45:59 -0300

    Update Panel.jsx

--- client/src/components/Admin/Users/Panel/Panel.jsx
+++ client/src/components/Admin/Users/Panel/Panel.jsx
@@ -1,7 +1,7 @@
 import { FaRegUserCircle } from "react-icons/fa";
 import { motion } from "framer-motion";
 
-export default function Panel() {
+export default function Panel({ userById }) {
   return (
     <div className="flex flex-col gap-2 items-center justify-center w-full h-full bg-[#282828] rounded-lg">
       <motion.div
@@ -14,8 +14,8 @@ export default function Panel() {
           <FaRegUserCircle className="w-40 h-40" />
         </div>
         <div>
-          <h2 className="text-[2rem] font-semibold">
-            Bienvenido Administrador
+          <h2 className="text-[2rem] font-semibold capitalize">
+            Bienvenido {userById.name + " " + userById.lastname}
           </h2>
         </div>
       </motion.div>
