7d28d2945d4b020376ec22077ddb3b2b0cb723e8
Jonavoe <voeffray.jonathan@gmail.com>
2023-08-25 15:13:45 -0300

    Update usersRouter.js

--- api/src/routes/usersRouter.js
+++ api/src/routes/usersRouter.js
@@ -6,16 +6,23 @@ const {
   setProgressUserHandler,
   getProgressUserHandler,
   getAllUserHandler,
+  editUserByIdHandler,
 } = require("../Handlers/usersHandlers");
 
 const UsersRouter = Router();
-
+//GET
 UsersRouter.get("/", getAllUserHandler);
 UsersRouter.get("/progress", getProgressUserHandler);
 UsersRouter.get("/email", getUserByLoginHandler);
+
+//PUT
 UsersRouter.put("/progress", setProgressUserHandler);
 UsersRouter.put("/access", setAccessUserHandler);
+UsersRouter.put("/edituser", editUserByIdHandler);
 
+//POST
 UsersRouter.post("/", postUsersHandler);
+
+//DELETE
 
 module.exports = UsersRouter;
