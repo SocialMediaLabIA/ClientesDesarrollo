ad5e82dda963a4243fa64b0ea664ebccbe1ea33d
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-08-25 15:06:34 -0300

    Update ProgressHeaders.jsx

--- client/src/components/RoadMapItems/ProgressHeaders.jsx
+++ client/src/components/RoadMapItems/ProgressHeaders.jsx
@@ -4,7 +4,6 @@ import { GrDocumentText } from "react-icons/gr";
 import { motion } from "framer-motion";
 
 export default function ProgressHeaders({ progressNumber }) {
-
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
   useEffect(() => {
@@ -30,7 +29,7 @@ export default function ProgressHeaders({ progressNumber }) {
       transition={{ duration: 0.5 }}
       className="flex justify-center items-center w-full h-5/5 mt-12 text-white"
     >
-{pages[progressNumber] && pages[progressNumber].video === true && (
+      {pages[progressNumber] && pages[progressNumber].video === true && (
         <iframe
           title="YouTube Video"
           width="580"
@@ -40,9 +39,6 @@ export default function ProgressHeaders({ progressNumber }) {
           allowFullScreen // Cambiado a allowFullScreen
           className="mt-5 w-5/6"
         />
-      )}
-      {pages[progressNumber] && pages[progressNumber].resume === true && pages[progressNumber] && pages[progressNumber].number !== 34 && (
-        <p className="mt-10 w-5/6 text-justify items-center">{pages[progressNumber] && pages[progressNumber].resumeText}</p>
       )}
       {pages[progressNumber] && pages[progressNumber].meet === true && (
         <div className="flex justify-center items-center h-full w-full ">
@@ -54,19 +50,30 @@ export default function ProgressHeaders({ progressNumber }) {
           />
         </div>
       )}
-      {pages[progressNumber] && pages[progressNumber].document === true && (
-        <div className="flex justify-center items-center h-full w-full ">
-          <GrDocumentText/>
-          <p>{pages[progressNumber].documentName}</p>
-        </div>
-      )}
+
+      <div className="flex flex-col justify-center items-center h-full w-full">
+        {pages[progressNumber] &&
+          pages[progressNumber].resume === true &&
+          pages[progressNumber] &&
+          pages[progressNumber].number !== 34 && (
+            <p className="mt-10 w-5/6 text-justify items-center text-sm">
+              {pages[progressNumber] && pages[progressNumber].resumeText}
+            </p>
+          )}
+        {pages[progressNumber] && pages[progressNumber].document === true && (
+          <div className="flex justify-center items-center h-full w-full mt-10">
+            <GrDocumentText className="h-6 w- mr-2"/>
+            <p>{pages[progressNumber].documentName}</p>
+          </div>
+        )}
+      </div>
+
       {pages[progressNumber] && pages[progressNumber].number === 34 && (
-   
         <img
-            src={pages[progressNumber] && pages[progressNumber].meetImage}
-            alt="imagen meet"
-            className="w-[380px] mt-16"
-          />
+          src={pages[progressNumber] && pages[progressNumber].meetImage}
+          alt="imagen meet"
+          className="w-[380px] mt-16"
+        />
       )}
     </motion.div>
   );
