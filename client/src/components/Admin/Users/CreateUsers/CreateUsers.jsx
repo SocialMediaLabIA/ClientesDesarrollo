7352b2cc09962f5a7040864f2b4f54709fa9f1b8
Jonavoe <voeffray.jonathan@gmail.com>
2023-08-25 15:54:24 -0300

    Update CreateUsers.jsx

--- client/src/components/Admin/Users/CreateUsers/CreateUsers.jsx
+++ client/src/components/Admin/Users/CreateUsers/CreateUsers.jsx
@@ -1,14 +1,20 @@
 import CreateUserForm from "./CreateUserForm";
+import { motion } from "framer-motion";
 
 export default function CreateUsers() {
   return (
     <div className="flex flex-col gap-2 items-center justify-center w-full h-full bg-[#282828] rounded-lg">
-      <div className="flex flex-col items-center justify-center gap-5">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 2 }}
+        className="flex flex-col items-center justify-center gap-5"
+      >
         <h2 className="text-[2rem] font-semibold">
           Formulario Creación Usuario
         </h2>
         <CreateUserForm />
-      </div>
+      </motion.div>
     </div>
   );
 }
