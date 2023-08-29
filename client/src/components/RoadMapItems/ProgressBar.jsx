3b2d3b8c8d5dbcc6760a72c7582bd140f1ac8256
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-08-29 18:20:25 -0300

    Update ProgressBar.jsx

--- client/src/components/RoadMapItems/ProgressBar.jsx
+++ client/src/components/RoadMapItems/ProgressBar.jsx
@@ -4,20 +4,20 @@ export default function ProgressBar({ progressNumber }) {
   // const [progressNumber, setProgress] = useState([0,0])
 
   return (
-    <div className="  flex flex-col justify-center items-center text-white text-sm  md:mt-1 p-2 bg-[#414040] rounded-lg">
+    <div className="  flex flex-col justify-start items-center text-white text-sm md:mt-1 p-2 bg-[#c905faad] rounded-lg overflow-scroll no-scrollbar  max-h-[400px]">
       {/* Seccion 1 */}
       <p
         className={
           progressNumber >= 1
-            ? "border-4 border-[#00DFFD] bg-[#00dffd5d] rounded-full w-8 h-8 flex justify-center items-center"
-            : "border-4 rounded-full w-8 h-8 flex justify-center items-center"
+            ? "border-4 border-[#00DFFD] bg-[#00dffd5d] rounded-full w-8 h-8 p-2 flex justify-center items-center"
+            : "border-4 rounded-full w-8 h-8 flex justify-center items-center p-2"
         }
       >
         1
       </p>
       <div
         className={
-          progressNumber >= 1 ? "border-2 border-[#00DFFD] w-1" : "border-2 w-1 "
+          progressNumber >= 1 ? "border-2 border-[#00DFFD] w-1" : "border-2 w-1"
         }
       ></div>
       <div
@@ -88,8 +88,8 @@ export default function ProgressBar({ progressNumber }) {
       <p
         className={
           progressNumber >= 11
-            ? "border-4 border-[#00DFFD] bg-[#00dffd5d] rounded-full w-8 h-8 flex justify-center items-center"
-            : "border-4 rounded-full w-8 h-8 flex justify-center items-center"
+            ? "border-4 border-[#00DFFD] bg-[#00dffd5d] rounded-full w-8 h-8 flex justify-center items-center p-2 p-2 "
+            : "border-4 rounded-full w-8 h-8 flex justify-center items-center p-2"
         }
       >
         2
@@ -167,8 +167,8 @@ export default function ProgressBar({ progressNumber }) {
       <p
         className={
           progressNumber >= 21
-            ? "border-4 border-[#00DFFD] bg-[#00dffd5d] rounded-full w-8 h-8 flex justify-center items-center"
-            : "border-4 rounded-full w-8 h-8 flex justify-center items-center"
+            ? "border-4 border-[#00DFFD] bg-[#00dffd5d] rounded-full w-8 h-8 flex justify-center items-center p-2  "
+            : "border-4 rounded-full w-8 h-8 flex justify-center items-center p-2"
         }
       >
         3
@@ -225,8 +225,8 @@ export default function ProgressBar({ progressNumber }) {
       <p
         className={
           progressNumber >= 28
-            ? "border-4 border-[#00DFFD] bg-[#00dffd5d] rounded-full w-8 h-8 flex justify-center items-center"
-            : "border-4 rounded-full w-8 h-8 flex justify-center items-center"
+            ? "border-4 border-[#00DFFD] bg-[#00dffd5d] rounded-full w-8 h-8 flex justify-center items-center p-2"
+            : "border-4 rounded-full w-8 h-8 flex justify-center items-center p-2"
         }
       >
         4
@@ -241,8 +241,8 @@ export default function ProgressBar({ progressNumber }) {
       <p
         className={
           progressNumber >= 29
-            ? "border-4 border-[#00DFFD] bg-[#00dffd5d] rounded-full w-8 h-8 flex justify-center items-center"
-            : "border-4 rounded-full w-8 h-8 flex justify-center items-center"
+            ? "border-4 border-[#00DFFD] bg-[#00dffd5d] rounded-full w-8 h-8 flex justify-center items-center p-2"
+            : "border-4 rounded-full w-8 h-8 flex justify-center items-center p-2"
         }
       >
         5
@@ -284,8 +284,8 @@ export default function ProgressBar({ progressNumber }) {
             <p
         className={
           progressNumber >= 34
-            ? "border-4 border-[#00DFFD] bg-[#00dffd5d] rounded-full w-8 h-8 flex justify-center items-center"
-            : "border-4 rounded-full w-8 h-8 flex justify-center items-center"
+            ? "border-4 border-[#00DFFD] bg-[#00dffd5d] rounded-full w-8 h-8 flex justify-center items-center p-2"
+            : "border-4 rounded-full w-8 h-8 flex justify-center items-center p-2"
         }
       >
         6
@@ -302,8 +302,8 @@ export default function ProgressBar({ progressNumber }) {
             <p
         className={
           progressNumber >= 35
-            ? "border-4 border-[#00DFFD] bg-[#00dffd5d] rounded-full w-8 h-8 flex justify-center items-center"
-            : "border-4 rounded-full w-8 h-8 flex justify-center items-center"
+            ? "border-4 border-[#00DFFD] bg-[#00dffd5d] rounded-full w-8 h-8 flex justify-center items-center p-2"
+            : "border-4 rounded-full w-8 h-8 flex justify-center items-center p-2"
         }
       >
         7
@@ -332,12 +332,67 @@ export default function ProgressBar({ progressNumber }) {
             <p
         className={
           progressNumber >= 38
-            ? "border-4 border-[#00DFFD] bg-[#00dffd5d] rounded-full w-8 h-8 flex justify-center items-center"
-            : "border-4 rounded-full w-8 h-8 flex justify-center items-center"
+            ? "border-4 border-[#00DFFD] bg-[#00dffd5d] rounded-full w-8 h-8 flex justify-center items-center p-2"
+            : "border-4 rounded-full w-8 h-8 flex justify-center items-center p-2"
         }
       >
         8
       </p>
+            <p
+        className={
+          progressNumber >= 38
+            ? "border-4 border-[#00DFFD] bg-[#00dffd5d] rounded-full w-8 h-8 flex justify-center items-center p-2"
+            : "border-4 rounded-full w-8 h-8 flex justify-center items-center p-2"
+        }
+      >
+        8
+      </p>
+            <p
+        className={
+          progressNumber >= 38
+            ? "border-4 border-[#00DFFD] bg-[#00dffd5d] rounded-full w-8 h-8 flex justify-center items-center p-2"
+            : "border-4 rounded-full w-8 h-8 flex justify-center items-center p-2"
+        }
+      >
+        8
+      </p>
+            <p
+        className={
+          progressNumber >= 38
+            ? "border-4 border-[#00DFFD] bg-[#00dffd5d] rounded-full w-8 h-8 flex justify-center items-center p-2"
+            : "border-4 rounded-full w-8 h-8 flex justify-center items-center p-2"
+        }
+      >
+        8
+      </p>
+            <p
+        className={
+          progressNumber >= 38
+            ? "border-4 border-[#00DFFD] bg-[#00dffd5d] rounded-full w-8 h-8 flex justify-center items-center p-2"
+            : "border-4 rounded-full w-8 h-8 flex justify-center items-center p-2"
+        }
+      >
+        8
+      </p>
+            <p
+        className={
+          progressNumber >= 38
+            ? "border-4 border-[#00DFFD] bg-[#00dffd5d] rounded-full w-8 h-8 flex justify-center items-center p-2"
+            : "border-4 rounded-full w-8 h-8 flex justify-center items-center p-2"
+        }
+      >
+        8
+      </p>
+            <p
+        className={
+          progressNumber >= 38
+            ? "border-4 border-[#00DFFD] bg-[#00dffd5d] rounded-full w-8 h-8 flex justify-center items-center p-2"
+            : "border-4 rounded-full w-8 h-8 flex justify-center items-center p-2"
+        }
+      >
+        8
+      </p>
+
     </div>
   );
 }
