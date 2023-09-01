0c252c35e6fe20dede769946d4417dbcf80cf130
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-09-01 17:45:07 -0300

    Update ProgressBar.jsx

--- client/src/components/RoadMapItems/ProgressBar.jsx
+++ client/src/components/RoadMapItems/ProgressBar.jsx
@@ -9,12 +9,21 @@ import { PiFlagPennantFill } from "react-icons/pi";
 import pages from "../../utils/progressPages";
 import { useDispatch, useSelector } from "react-redux";
 
-export default function ProgressBar({ progressNumber }) {
+export default function ProgressBar({ progress, setProgress, setLoader }) {
   const [openBar, setOpenBar] = useState(false);
 
   const setOpenBarHandler = () => {
     setOpenBar(!openBar);
   };
+
+  const pressProgressHandler = (number) => {
+    setInterval(() => {
+      setLoader(false)
+    }, 3000);
+setLoader(true)
+    setProgress(number)
+
+  }
 
   return (
     <div
@@ -33,27 +42,34 @@ export default function ProgressBar({ progressNumber }) {
               return (
                 <div
                   key={index}
-                  className="my-2 ml-3 flex justify-center items-center w-fit"
+                  className="my-2 ml-3 flex justify-center items-center"
                 >
-                  {item.number < progressNumber && (
-                    <PiFlagPennantFill className="text-[#00DFFD]" />
+                  {item.number < progress && (
+                    <PiFlagPennantFill 
+                    onClick={() => {pressProgressHandler(item.number)}}
+                    className="text-[#00DFFD]" />
                   )}
-                  {item.number > progressNumber && (
-                    <PiFlagPennantFill className="text-[#fafafa]" />
+                  {item.number > progress && (
+                    <PiFlagPennantFill 
+                    onClick={() => {pressProgressHandler(item.number)}}
+                    className="text-[#fafafa]" />
                   )}
-                  {item.number === progressNumber && (
-                    <PiFlagPennantFill className="text-[#00DFFD]" />
+                  {item.number === progress && (
+                    <PiFlagPennantFill 
+                    onClick={() => {pressProgressHandler(item.number)}}
+                    className="text-[#00DFFD]" />
                   )}
-
+                  {/* TITULOS BANDERAS */}
                   <div
                     className={
                       openBar
-                        ? "ml-5 flex justify-start items-center border-b-2 border-[#beb8b827] hover:border-[#beb8b84f] w-[300px] h-5 cursor-pointer"
-                        : "h-5 ml-5 flex justify-start items-center w-[300px] hover:border-[#beb8b84f] cursor-pointer"
+                        ? "ml-5 flex justify-start items-center border-b-2 border-[#beb8b827] hover:border-[#beb8b84f] w-[280px] h-5 cursor-pointer "
+                        : "h-5 ml-5 flex justify-start items-center w-fit border-b-2 hover:border-[#beb8ba4f] cursor-pointer"
                     }
+                    onClick={() => {pressProgressHandler(item.number)}}
                   >
                     {openBar ? (
-                      <h1 className="h-5 p-1">{item.title.toUpperCase()}</h1>
+                      <h1 className="h-5 p-1 hover:mb-1">{item.title.toUpperCase()}</h1>
                     ) : (
                       <h1 className="h-5"></h1>
                     )}
@@ -62,60 +78,69 @@ export default function ProgressBar({ progressNumber }) {
               );
             } else {
               return (
-                <>
+                <React.Fragment key={`fragment-${index}`}>
                   {
                     <div
-                      key={index}
+                      
                       className={
-                        item.number === progressNumber && openBar
-                          ? "ml-2 flex justify-center items-center w-fit bg-[#ffffff42] hover:bg-[#b8b3b342] p-1 rounded-md cursor-pointer"
-                          : "ml-2 flex justify-center items-center w-fit cursor-pointer hover:bg-[#b8b3b342]"
+                        item.number === progress && openBar
+                          ? "ml-2 flex justify-center items-center w-fit  bg-[#ffffff42] hover:bg-[#b8b3b342] p-1 rounded-md  cursor-pointer"
+                          : "ml-2 flex justify-center items-center w-fit cursor-pointer "
                       }
                     >
-                      {item.number === progressNumber && (
-                        <p className="my-1 border-4 border-[#4de75a] bg-[#1bff1352] rounded-full w-6 h-6 p-2  flex justify-center items-center"></p>
+                      {item.number === progress && (
+                        <p 
+                        onClick={() => {pressProgressHandler(item.number)}}
+                        className=" my-1 border-4 border-[#4de75a] bg-[#1bff1352] rounded-full w-6 h-6 p-2  flex justify-center items-center hover:scale-110 cursor-pointer"></p>
                       )}
-                      {item.number < progressNumber && (
-                        <p className="my-1 border-4 border-[#00DFFD] bg-[#00dffd5d] rounded-full w-6 h-6 p-2  flex justify-center items-center"></p>
+                      {item.number < progress && (
+                        <p 
+                        onClick={() => {pressProgressHandler(item.number)}}
+                        className="my-1 border-4 border-[#00DFFD] bg-[#00dffd5d] rounded-full w-6 h-6 p-2  flex justify-center items-center hover:scale-110"></p>
                       )}
-                      {item.number > progressNumber && (
-                        <p className="my-1 border-4 rounded-full w-6 h-6 flex justify-center items-center p-2"></p>
+                      {item.number > progress && (
+                        <p 
+                        onClick={() => {pressProgressHandler(item.number)}}
+                        className="my-1 border-4 rounded-full w-6 h-6 flex justify-center items-center p-2 hover:scale-110"></p>
                       )}
 
+                      {/* TITULOS */}
                       <div
                         className={
                           openBar
-                            ? "ml-5 flex justify-start items-center border-b-2 border-[#beb8b827] w-[300px] h-5"
-                            : "h-5 ml-5 flex justify-start items-center w-[300px]"
+                            ? "ml-5 flex justify-start items-center border-b-2 border-[#beb8b827] w-[280px] h-5 hover:mb-1 cursor-pointer"
+                            : "h-5 ml-5 flex justify-start items-center   cursor-pointer "
                         }
+                        onClick={() => {pressProgressHandler(item.number)}}
                       >
                         {" "}
                         {openBar ? (
-                          <h1 className="h-5">{item.title}</h1>
+                          <h1 className="h-5 w-[300px]">{item.title}</h1>
                         ) : (
                           <h1 className="h-5"></h1>
                         )}
                       </div>
                     </div>
                   }
-                </>
+                </React.Fragment>
               );
             }
           })}
         </div>
       </div>
+      {/* BARRA APERTURA */}
       <div
         className={
           openBar
-            ? "absolute flex items-center justify-end h-fit  text-white w-[370px] transition-transform transform translate-x-0 duration-500 ease-in-out delay-150"
-            : "absolute flex items-center justify-end h-fit  text-white w-[60px] transition-transform transform translate-x-0 duration-500 ease-in-out delay-150"
+            ? "absolute right-0 cursor-pointer  flex items-center justify-end h-[500px] md:h-screen  text-white hover:bg-[#a048b6bd] rounded-r-xl"
+            : "absolute right-0 cursor-pointer  flex items-center justify-end h-[500px]  md:h-screen text-white hover:bg-[#a048b6b4] rounded-r-xl"
         }
         onClick={setOpenBarHandler}
       >
         {openBar ? (
-          <HiChevronLeft className="w-5 h-5  bg-[#6b277c] rounded-full" />
+          <HiChevronLeft className="w-5 h-5   rounded-full " />
         ) : (
-          <HiChevronRight className="w-5 h-5  bg-[#6b277c] rounded-full" />
+          <HiChevronRight className="w-5 h-5   rounded-full" />
         )}
       </div>
     </div>
