251c181cc76dfc3d26325eadd12cc0844ee6be15
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-09-01 17:46:05 -0300

    Update ProgressBar.jsx

--- client/src/components/RoadMapItems/ProgressBar.jsx
+++ client/src/components/RoadMapItems/ProgressBar.jsx
@@ -18,13 +18,11 @@ export default function ProgressBar({ progress, setProgress, setLoader }) {
 
   const pressProgressHandler = (number) => {
     setTimeout(() => {
-      setLoader(false)
+      setLoader(false);
     }, 3000);
-
-setLoader(true)
-    setProgress(number)
-
-  }
+    setLoader(true);
+    setProgress(number);
+  };
 
   return (
     <div
@@ -46,19 +44,28 @@ setLoader(true)
                   className="my-2 ml-3 flex justify-center items-center"
                 >
                   {item.number < progress && (
-                    <PiFlagPennantFill 
-                    onClick={() => {pressProgressHandler(item.number)}}
-                    className="text-[#00DFFD]" />
+                    <PiFlagPennantFill
+                      onClick={() => {
+                        pressProgressHandler(item.number);
+                      }}
+                      className="text-[#00DFFD]"
+                    />
                   )}
                   {item.number > progress && (
-                    <PiFlagPennantFill 
-                    onClick={() => {pressProgressHandler(item.number)}}
-                    className="text-[#fafafa]" />
+                    <PiFlagPennantFill
+                      onClick={() => {
+                        pressProgressHandler(item.number);
+                      }}
+                      className="text-[#fafafa]"
+                    />
                   )}
                   {item.number === progress && (
-                    <PiFlagPennantFill 
-                    onClick={() => {pressProgressHandler(item.number)}}
-                    className="text-[#00DFFD]" />
+                    <PiFlagPennantFill
+                      onClick={() => {
+                        pressProgressHandler(item.number);
+                      }}
+                      className="text-[#00DFFD]"
+                    />
                   )}
                   {/* TITULOS BANDERAS */}
                   <div
@@ -67,10 +74,14 @@ setLoader(true)
                         ? "ml-5 flex justify-start items-center border-b-2 border-[#beb8b827] hover:border-[#beb8b84f] w-[280px] h-5 cursor-pointer "
                         : "h-5 ml-5 flex justify-start items-center w-fit border-b-2 hover:border-[#beb8ba4f] cursor-pointer"
                     }
-                    onClick={() => {pressProgressHandler(item.number)}}
+                    onClick={() => {
+                      pressProgressHandler(item.number);
+                    }}
                   >
                     {openBar ? (
-                      <h1 className="h-5 p-1 hover:mb-1">{item.title.toUpperCase()}</h1>
+                      <h1 className="h-5 p-1 hover:mb-1">
+                        {item.title.toUpperCase()}
+                      </h1>
                     ) : (
                       <h1 className="h-5"></h1>
                     )}
@@ -82,7 +93,6 @@ setLoader(true)
                 <React.Fragment key={`fragment-${index}`}>
                   {
                     <div
-                      
                       className={
                         item.number === progress && openBar
                           ? "ml-2 flex justify-center items-center w-fit  bg-[#ffffff42] hover:bg-[#b8b3b342] p-1 rounded-md  cursor-pointer"
@@ -90,19 +100,28 @@ setLoader(true)
                       }
                     >
                       {item.number === progress && (
-                        <p 
-                        onClick={() => {pressProgressHandler(item.number)}}
-                        className=" my-1 border-4 border-[#4de75a] bg-[#1bff1352] rounded-full w-6 h-6 p-2  flex justify-center items-center hover:scale-110 cursor-pointer"></p>
+                        <p
+                          onClick={() => {
+                            pressProgressHandler(item.number);
+                          }}
+                          className=" my-1 border-4 border-[#4de75a] bg-[#1bff1352] rounded-full w-6 h-6 p-2  flex justify-center items-center hover:scale-110 cursor-pointer"
+                        ></p>
                       )}
                       {item.number < progress && (
-                        <p 
-                        onClick={() => {pressProgressHandler(item.number)}}
-                        className="my-1 border-4 border-[#00DFFD] bg-[#00dffd5d] rounded-full w-6 h-6 p-2  flex justify-center items-center hover:scale-110"></p>
+                        <p
+                          onClick={() => {
+                            pressProgressHandler(item.number);
+                          }}
+                          className="my-1 border-4 border-[#00DFFD] bg-[#00dffd5d] rounded-full w-6 h-6 p-2  flex justify-center items-center hover:scale-110"
+                        ></p>
                       )}
                       {item.number > progress && (
-                        <p 
-                        onClick={() => {pressProgressHandler(item.number)}}
-                        className="my-1 border-4 rounded-full w-6 h-6 flex justify-center items-center p-2 hover:scale-110"></p>
+                        <p
+                          onClick={() => {
+                            pressProgressHandler(item.number);
+                          }}
+                          className="my-1 border-4 rounded-full w-6 h-6 flex justify-center items-center p-2 hover:scale-110"
+                        ></p>
                       )}
 
                       {/* TITULOS */}
@@ -112,7 +131,9 @@ setLoader(true)
                             ? "ml-5 flex justify-start items-center border-b-2 border-[#beb8b827] w-[280px] h-5 hover:mb-1 cursor-pointer"
                             : "h-5 ml-5 flex justify-start items-center   cursor-pointer "
                         }
-                        onClick={() => {pressProgressHandler(item.number)}}
+                        onClick={() => {
+                          pressProgressHandler(item.number);
+                        }}
                       >
                         {" "}
                         {openBar ? (
