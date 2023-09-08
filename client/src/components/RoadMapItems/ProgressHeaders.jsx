df5f4af34b392fb1e9b4157c796f9172949663ee
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-09-08 14:46:44 -0300

    Update ProgressHeaders.jsx

--- client/src/components/RoadMapItems/ProgressHeaders.jsx
+++ client/src/components/RoadMapItems/ProgressHeaders.jsx
@@ -29,43 +29,58 @@ export default function ProgressHeaders({ progress }) {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 0.5 }}
-      className="flex justify-center items-center w-full  text-white z-0"
+      className="flex justify-center items-center w-full  text-white z-0 "
     >
       {/* VIDEOS */}
       {pages[progress] && pages[progress].video === true && (
-        <iframe
+        <motion.iframe
+          initial={{ y: 0 }}
+          animate={{ y: 0 }}
+          transition={{ duration: 0.5 }}
           title="YouTube Video"
           width="580"
           height="330"
           src={pages[progress] && pages[progress].videoSource} // Cambia esta URL por la URL correcta del video embebido
           frameBorder="0" // Cambiado a frameBorder
           allowFullScreen // Cambiado a allowFullScreen
-          className=" w-[270px] md:w-[700px] md:h-[400px] xl:w-[1000px] xl:h-[500px]"
+          className=" w-[270px] md:w-[600px] md:h-[300px] xl:w-[800px] xl:h-[400px]"
         />
       )}
       {/* MEETS */}
-      {pages[progress] && pages[progress].meet === true && pages[progress].section === false && (
-        <div className="flex justify-center items-center h-full w-fit">
-          <img
-            src={selectedImage}
-            // src={pages[progress] && pages[progress].meetImage}
-            alt="imagen meet"
-            className=" w-[30%] md:w-[300px]  xl:w-[400px]  rounded-lg"
-          />
-        </div>
-      )}
+      {pages[progress] &&
+        pages[progress].meet === true &&
+        pages[progress].section === false && (
+          <motion.div
+            initial={{ x: 0 }}
+            animate={{ x: 0 }}
+            transition={{ duration: 0.5 }}
+            className="flex justify-center items-center h-full w-fit"
+          >
+            <img
+              src={selectedImage}
+              // src={pages[progress] && pages[progress].meetImage}
+              alt="imagen meet"
+              className=" w-[250px] md:w-[300px]  xl:w-[400px]  rounded-lg"
+            />
+          </motion.div>
+        )}
       {/* DOCUMENTS */}
       {pages[progress] &&
         pages[progress].document === true &&
         pages[progress].resume === true &&
-        pages[progress].number !== 38 && pages[progress].section === false && (
-          <div className="  flex flex-col justify-start items-center h-fit w-full max-w-[300px] md:max-w-[600px] ">
+        pages[progress].number !== 38 &&
+        pages[progress].section === false && (
+          <motion.div
+            initial={{ y: 0 }}
+            animate={{ y: 0 }}
+            transition={{ duration: 0.5 }}
+            className=" border-2  flex flex-col justify-start items-center h-fit w-full max-w-[300px] md:max-w-[600px] "
+          >
             <p className=" w-5/6 text-justify items-center text-sm md:text-base mb-5">
               {pages[progress] && pages[progress].resumeText}
             </p>
             <div className=" w-fit  max-w-[250px] flex flex-col justify-start items-start">
               {pages[progress].documentArray.map((item, index) => {
-
                 return (
                   <a
                     href={item.link}
@@ -80,7 +95,7 @@ export default function ProgressHeaders({ progress }) {
                 );
               })}
             </div>
-          </div>
+          </motion.div>
         )}
       {/* ULTIMA SECCION */}
       {pages[progress] && pages[progress].number === 43 && (
