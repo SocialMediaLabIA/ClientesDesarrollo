b48acfa1298d754657bf519d01f32a58b6645e0e
Jonavoe <voeffray.jonathan@gmail.com>
2023-09-08 16:22:59 -0300

    Update router.js

--- api/src/routes/router.js
+++ api/src/routes/router.js
@@ -1,10 +1,12 @@
 const { Router } = require("express");
 const UsersRouter = require("./usersRouter");
 const ContentsRouter = require("./contentsRouter");
+const PagesRouter = require("./pagesRouter");
 
 const router = Router();
 
 router.use("/users", UsersRouter);
 router.use("/contents", ContentsRouter);
+router.use("/pages", PagesRouter);
 
 module.exports = router;
